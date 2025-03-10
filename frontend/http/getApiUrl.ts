8d8ae43db8e137d20623630f12ffe113857da04c
warmergroup <ulugbekjonsultonov4@gmail.com>
2025-03-11 00:58:15 +0500

    editing 2

--- frontend/http/getApiUrl.ts
+++ frontend/http/getApiUrl.ts
@@ -1,4 +1,5 @@
 export const getApiUrl = () => {
-  console.log('process env fayli:', process);
+  console.log('process env fayli:', process.env);
   return process.env.NUXT_PUBLIC_API_URL || "http://localhost:8080";
+  // return process.env.NUXT_PUBLIC_API_URL || "http://localhost:8080";
 };
