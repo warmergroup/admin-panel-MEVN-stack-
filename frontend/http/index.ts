2816878d82dbf3cda0b09022ee41a9360f3bd77f
Ulugʻbek <88683560+warmergroup@users.noreply.github.com>
2025-03-24 11:57:21 +0500

    Update index.ts
    bug fixing

--- frontend/http/index.ts
+++ frontend/http/index.ts
@@ -1,9 +1,6 @@
 import axios from "axios";
 
-
 export const API_URL = 'http://localhost:8080'
-// export const API_URL = getApiUrl();
-// export const API_URL = getApi()
 
 const $axios = axios.create({
   withCredentials: true,
