1c9360e15aef3166797ad343b4916b3cdfb8c2b7
Ulugʻbek <88683560+warmergroup@users.noreply.github.com>
2025-03-25 14:22:56 +0500

    Update index.ts

--- frontend/http/index.ts
+++ frontend/http/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL = 'http://localhost:8080'
+export const API_URL = 'https://admin-panel-mevn-stack-backend.vercel.app'
 
 const $axios = axios.create({
   withCredentials: true,
