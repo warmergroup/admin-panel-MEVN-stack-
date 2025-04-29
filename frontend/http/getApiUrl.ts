d152f204e7d088ae675bce7227ecc516efdc862b
Sultonov Ulug'bek <ulugbekjonsultonov4@gmail.com>
2025-04-30 04:11:46 +0500

    Update env configuration

--- frontend/http/getApiUrl.ts
+++
@@ -1,6 +0,0 @@
-import { useNuxtApp } from '#app'
-
-export const getApiUrl = () => {
-  const { $config } = useNuxtApp()
-  return $config.public.apiBase
-}
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,13 +1,12 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
-  devtools: { enabled: true },
+  devtools: {enabled: true},
   modules: ["@nuxt/ui", "@nuxt/image", "@pinia/nuxt"],
   plugins: ["~/plugins/vue-query.ts"],
   runtimeConfig: {
     public: {
-      API_URL: process.env.NUXT_PUBLIC_API_URL || "http://localhost:8080",
-    },
-
+      API_URL: process.env.VITE_APP_FILE_API_URL
+    }
   },
   devServer: {
     host: '0.0.0.0',
