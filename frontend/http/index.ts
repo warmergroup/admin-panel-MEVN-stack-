042f1b20eb746a639f2268f9896d548945cce359
Sultonov Ulug'bek <ulugbekjonsultonov4@gmail.com>
2025-04-30 04:45:48 +0500

    bug fixing

--- frontend/http/index.ts
+++ frontend/http/index.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const API_URL = import.meta.env.VITE_APP_FILE_API_URL;
+export const API_URL = import.meta.env.VITE_APP_API_URL;
+console.log(API_URL);
 const $axios = axios.create({
   withCredentials: true,
   baseURL: `${API_URL}/api`,
