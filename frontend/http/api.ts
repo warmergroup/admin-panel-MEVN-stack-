a1974898dc2352bd11c50379dfe73c71b8c08fdc
Sultonov Ulug'bek <ulugbekjonsultonov4@gmail.com>
2025-04-30 05:56:06 +0500

    test localstorage 3

--- frontend/http/api.ts
+++ frontend/http/api.ts
@@ -1,50 +1,37 @@
 import axios from "axios";
 import {API_URL} from ".";
-import {useAuthStore} from "~/store/auth.store";
 
 const $api = axios.create({
   withCredentials: true,
   baseURL: `${API_URL}/api`,
-});
+})
 
-// REQUEST: Tokenni headerga qo‘shish
 $api.interceptors.request.use(config => {
   config.headers.Authorization = `Bearer ${localStorage.getItem('accessToken')}`
   return config
 })
 
-// RESPONSE: Token yangilash va qayta so‘rov
 $api.interceptors.response.use(
-  res => res,
+  config => {
+    return config
+  },
   async error => {
-    const originalRequest = error.config;
+    const originalRequest = error.config
 
-    if (error.response?.status === 401 && !originalRequest._isRetry) {
-      originalRequest._isRetry = true;
+    if (error.response.status === 401 && error.config && !error.config._isRetry) {
+      originalRequest._isRetry = true
+
       try {
-        const {data} = await axios.post(`${API_URL}/api/auth/refresh`, {}, {withCredentials: true});
-        localStorage.setItem('accessToken', data.accessToken);
-
-        // AuthStore yangilash
-        const authStore = useAuthStore();
-        authStore.setUser(data.user);
-        authStore.setIsAuth(true);
-
-        return $api.request(originalRequest);
-      } catch (err) {
-        console.warn('Refresh token ishlamadi:', err);
-
-        // Logout yoki router.push('/auth') qilish kerak bo'lishi mumkin
-        const authStore = useAuthStore();
-        authStore.setIsAuth(false);
-        authStore.setUser(null);
-        localStorage.removeItem('accessToken');
-
-        // Optional: router.push('/auth')
+        const {data} = await $axios.get('/auth/refresh')
+        localStorage.setItem('accessToken', data.accessToken)
+        return $api.request(originalRequest)
+      } catch (error) {
+        console.log('Not authorized')
       }
     }
-    throw error;
-  }
-);
 
-export default $api;
+    throw error
+  }
+)
+
+export default $api
