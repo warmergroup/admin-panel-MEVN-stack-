254b5509b1ec7faf61306593e8df7b5aac3f6f5a
Sultonov Ulug'bek <ulugbekjonsultonov4@gmail.com>
2025-04-30 05:47:46 +0500

    test localstorage

--- frontend/http/index.ts
+++ frontend/http/index.ts
@@ -1,9 +1,6 @@
 import axios from "axios";
-
-
-// export const API_URL = 'https://admin-panel-mevn-stack-backend.vercel.app'
-// export const API_URL = import.meta.env.VITE_APP_FILE_API_URL;
-export const API_URL = 'http://localhost:8080'
+  
+export const API_URL = import.meta.env.VITE_APP_FILE_API_URL;
 const $axios = axios.create({
   withCredentials: true,
   baseURL: `${API_URL}/api`,
